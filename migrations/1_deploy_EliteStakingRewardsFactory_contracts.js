7aed93fab84aa8c7775015059501cb8d5143b4e3
EthereumEliteswap <73964644+EthereumEliteswap@users.noreply.github.com>
2021-04-10 03:07:00 +0300

    Deploy EliteStakingRewardsFactory in bsc

--- migrations/1_deploy_EliteStakingRewardsFactory_contracts.js
+++ migrations/1_deploy_EliteStakingRewardsFactory_contracts.js
@@ -12,6 +12,12 @@ module.exports = async function (deployer, network, addresses) {
   } else if (network === 'ropsten' || network === 'ropsten-fork') {
     rewardsToken = await '0x380291A9A8593B39f123cF39cc1cc47463330b1F'; // eltAddress
     stakingRewardsGenesis = await 1605484800; // 2020-11-16 02:00:00 +0200
+  } else if (network === 'bsc_mainnet' || network === 'bsc_mainnet-fork') {
+    rewardsToken = await '0x380291A9A8593B39f123cF39cc1cc47463330b1F'; // bsc mainnet ELTB Contract
+    stakingRewardsGenesis = await 1618012800; // 2021-04-10 03:00:00 +0300
+  } else if (network === 'bsc_testnet' || network === 'bsc_testnet-fork') {
+    rewardsToken = await '0xbbAA1C83ea09b5cE0868E0171fE1898Cf7c3c291'; // bsc testnet ELTB Contract
+    stakingRewardsGenesis = await 1618012800; // 2021-04-10 03:00:00 +0300
   } else {
     throw new Error('No Elite Swap on this network')
   }
